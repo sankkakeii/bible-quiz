e307866c16db2ee3691900b8d57fd8759eba3baf
David Ikenna <sankkakeii@gmail.com>
2024-04-19 18:51:19 +0100

    add admin page

--- pages/spelling-instructions.js
+++ pages/spelling-instructions.js
@@ -12,9 +12,10 @@ export default function Instructions() {
 
 
     // Function to handle button(s) click
-    const handleQuizClicked = () => {
-        router.push('/quiz/bible-quiz')
+    const handleSpellingClicked = () => {
+        router.push('/quiz/spelling-bee')
     }
+
 
 
 
@@ -26,12 +27,12 @@ export default function Instructions() {
                     <h1 className="text-2xl font-semibold text-teal-700 text-center">INSTRUCTIONS</h1>
 
                     <div>
-                        <h1 className="text-2xl font-semibold text-blue-700 text-center">BIBLE QUIZ</h1>
-                        <h1 className="text-xl font-semibold text-teal-400 text-center">You are to answer the following questions as fast and correctly as you can. You have 20 minutes for the quiz section.</h1>
+                        <h1 className="text-2xl font-semibold text-blue-700 text-center">SPELLING BEE</h1>
+                        <h1 className="text-xl font-semibold text-teal-400 text-center">You are to listen to the audio and spell out the words as fast as you can. You have 20 minutes for the spelling bee section.</h1>
                     </div>
 
                     <div className="p-4 w-full flex flex-col gap-2">
-                        <Button onClick={handleQuizClicked} className="next-button bg-green-500 text-white py-4 md:py-6 px-6 md:px-12 rounded-md mt-4 md:mt-8">Begin Quiz</Button>
+                        <Button onClick={handleSpellingClicked} className="next-button bg-green-500 text-white py-4 md:py-6 px-6 md:px-12 rounded-md mt-4 md:mt-8">Begin Spelling Bee</Button>
                     </div>
                 </Card>
             </div>
